8b8a9ccdb8af95d71b88254cffea6883fe4d378c
Jan <jbieron@gmail.com>
2013-12-02 18:25:13 +0100

    a legend is born...

---
+++ Civilization/Model/Cell.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Civilization.Model {
+    class Cell {
+        List<Cell> neighbors;
+        double desirability, defensibility;
+        Dictionary<Civ, double> capitalDistance;
+        Civ owner;
+    }
+}
